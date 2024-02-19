2ba983f2b235eb0eb96d770f9d618b50c57b597a
BobbySmurf <135199516+BobbySmurf@users.noreply.github.com>
2024-02-18 22:45:44 -0600

    123

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+
+import ParticlesComponent from './components/particles';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ParticlesComponent id="particles" />
+      <h1>Particles are so fun!</h1>
     </div>
   );
 }
